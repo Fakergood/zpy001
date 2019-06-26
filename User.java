8ae4ce7686f80eecc4eb788166a4bf28f09e93a2
Fakergood <274248206@qq.com>
2019-06-26 16:06:22 +0800

    222

---
+++ User.java
@@ -0,0 +1,21 @@
+package com.zpy.springbootdemo2.entity;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private String id;
+    private String name;
+    private Integer age;
+    private Date  createtime;
+}
